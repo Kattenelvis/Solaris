b5b4975404b7d7b36445c027a1c55de9d0cad656
Kattenelvis <officialkattenelvis@gmail.com>
2020-09-06 19:05:55 +0200

    economics

--- Assets/Scripts/EarthMarket.cs
+++ Assets/Scripts/EarthMarket.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-//using UnityEngine;
+using UnityEngine;
+public struct Product
+{
+    float price;
+    public float demand;
+    public float supply;
 
+
+
+}
 public class EarthMarket : MonoBehaviour
 {
     public Product[] products;
 
 
-
-}
-
-struct Product
-{
-    public float price;
 }
