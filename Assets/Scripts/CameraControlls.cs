ce0baf2a7463ed4b7c478bfdb6855a1f95f73507
Kattenelvis <officialkattenelvis@gmail.com>
2020-09-06 18:57:26 +0200

    updated version

---
+++ Assets/Scripts/CameraControlls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlls : MonoBehaviour
+{
+
+    public Camera mainCamera;
+    public float panSenitivity;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cameraXposition = mainCamera.transform.position.x + Input.GetAxis("Horizontal") * panSenitivity;
+        float cameraYposition = mainCamera.transform.position.y;
+        float cameraZposition = mainCamera.transform.position.z + Input.GetAxis("Vertical") * panSenitivity;
+
+        mainCamera.transform.position = new Vector3(cameraXposition, cameraYposition, cameraZposition);
+
+    }
+}
---
+++ Assets/Scripts/EarthMarket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+
+public class EarthMarket : MonoBehaviour
+{
+    public Product[] products;
+
+
+
+}
+
+struct Product
+{
+    public float price;
+}
